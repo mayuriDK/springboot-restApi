45521232a95bb075b9a2da37cfef3b7a19b283ed
Mayuri <mayuri.kate@intellectdesign.com>
2021-04-25 14:41:24 +0530

    First Commit

---
+++ src/main/java/com/app/restservices/SpringBootStartupApplication.java
@@ -0,0 +1,13 @@
+package com.app.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStartupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/restservices/SpringBootStartupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
